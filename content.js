e7bc03f50d21cd863c7a3e741e49187479b77b10
Ramin <118001668+Ramin2009Mc@users.noreply.github.com>
2024-06-29 12:24:21 +0500

    Update content.js

--- content.js
+++ content.js
@@ -1,13 +1,17 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.action === 'placeBet') {
-	  const betAmountInput = document.querySelector('input[name="bet_value"]');
-	  if (betAmountInput) {
-		betAmountInput.value = request.amount;
-	  }
-  
-	  const placeBetButton = document.querySelector('button[name="submit_bet"]');
-	  if (placeBetButton) {
-		placeBetButton.click();
-	  }
-	}
-  });
+    if (request.action === 'placeBet') {
+        const betAmountInput = document.querySelector('input[name="bet_value"]');
+        if (betAmountInput) {
+            betAmountInput.value = request.amount;
+        } else {
+            console.error('Bet amount input not found');
+        }
+
+        const placeBetButton = document.querySelector('button[name="submit_bet"]');
+        if (placeBetButton) {
+            placeBetButton.click();
+        } else {
+            console.error('Place bet button not found');
+        }
+    }
+});
